47981245c1278719f6e3c644c7f13f7dc7711a08
André <andrekeny@gmail.com>
2019-05-06 10:08:41 +0200

    create insert_All

---
+++ insert_All.sql
@@ -0,0 +1,31 @@
+insert into expert ( nom, prenom, email, nombreprojet, dateembauche, fonction)
+values ('keny','andre','andrekeny@gmail.com','10','2018-06-05 20:00:00','CODEUR');
+insert into expert ( nom, prenom, email, nombreprojet, dateembauche, fonction)
+values ('ouhenia','nassim','nassimouhenia@gmail.com','0','2017-06-05 20:00:00','DECISION');
+--Developpeur
+insert into Developpeur (nom,prenom,email) values ('kenzo','andre','kenzo12@gmail.com');
+insert into Developpeur (nom,prenom,email) values ('sadio','mathilde','mathilde12@gmail.com');
+
+--Beneficiaire
+insert into Beneficiaire (nom,prenom,email) values ('sakho','binta','binta@gmail.com');
+insert into Beneficiaire (nom,prenom,email) values ('amiton','eric','eric@gmail.com');
+insert into Beneficiaire (nom,prenom,email) values ('amiton','marc','marc@gmail.com');
+insert into Beneficiaire (nom,prenom,email) values ('amiton','jeanne','jeanne@gmail.com');
+
+--Personne
+insert into Personne (nom,prenom,email) values('ndoye','mareme','mareme@gmail.com');
+insert into Personne (nom,prenom,email) values('ndoye','oumy','oumy@gmail.com');
+insert into Personne (nom,prenom,email) values('ndoye','mohamed','mohamed@gmail.com');
+
+--Local
+insert into Local (nom,capacite) values ('Port Real','150');
+insert into Local (nom,capacite) values ('Winterfel','250');
+insert into Local (nom,capacite) values ('Westeros','100');
+insert into Local (nom,capacite) values ('Nord','50');
+insert into Local (nom,capacite) values ('Rome','25');
+
+--Projet
+insert into Projet (nom,description) values ('coavionnage','site de coavionnage pour mettre en relations pilotes
+  et passagers');
+insert into Projet (nom,description) values ('lecteur RSS','application pour lire les flux rss');
+insert into Projet (nom,description) values ('jeu de dame','application de jeu de dame et echec');
